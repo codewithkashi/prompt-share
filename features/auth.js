e5d61f005636481dcc8e705873fd059d82c5404c
Muhammad Usman Zia <smusmanzia@gmail.com>
2023-12-28 16:30:12 +0500

    api fix

--- features/auth.js
+++ features/auth.js
@@ -5,21 +5,19 @@ connectDB();
 export const isAuth = async (req, res) => {
   const { token } = req.cookies;
   if (!token) {
-    res.json({
+    return res.json({
       success: false,
       message: "Login First",
     });
-    return false;
   }
   const decodedToken = jwt.verify(token, process.env.SECRET_TOKEN);
   const user = await User.findOne({ _id: decodedToken._id });
   if (user) {
     return user;
   } else {
-    res.json({
+    return res.json({
       success: false,
       message: "Invalid Cookie",
     });
-    return false;
   }
 };
